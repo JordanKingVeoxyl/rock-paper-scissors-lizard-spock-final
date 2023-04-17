e5f801fd2848a3472d72a5488f044d588c0bdfa4
JordanKingVeoxyl <veoxyl@gmail.com>
2023-04-17 14:40:09 +0000

    Added the variables which our JavaScript functions will change and increment based on the user and computer choices. Also set up the ability for the computer to make a random selection based 5 different variables. This function will be responsible for choosing the computer choice that will play against the user's choice.

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+//Code from "What's dev" as specified in ReadMe
+// Varibles for DOM elements
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+const lizard_div = document.getElementById("lizard");
+const spock_div = document.getElementById("spock");
+
+//GETS RANDOM COMPUTER CHOICE
+function getComputerChoice() {
+	const choices = ['r', 'p', 's', 'l', 'k'];
+	const randomNumber = Math.floor(Math.random() * 5);
+	return choices[randomNumber];
+}
